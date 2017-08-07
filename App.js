19fbeb5ef8a600084c582fcfd953b4e6f3a1822f
BowenTH <1578779013@qq.com>
2017-08-07 21:49:56 +0800

    2017年8月7日 21:48:27 添加注释 App.js

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './style/App.css';
 import imgJson from './data/img.json';
 
+/* 头部的图片展示 */
 class App extends Component{
     render(){
         return (
@@ -17,10 +18,12 @@ class App extends Component{
 
 class Hello extends Component{
     render(){
-        return(<i>this is a I</i>);
+        return(<i>this is a Bowen</i>);
     }
 }
 
+// 第一种方式是默认暴露 可赋值给任意变量
+// 第二种方式可暴露多个变量，名称要一致
 // export default App;
 export {Hello};
 export {App};
